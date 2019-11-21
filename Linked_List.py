f0da62879f05103e27035770a29bb3bb9474e7ed
Saurabh Jaiswal <saurabhchris1@gmail.com>
2019-11-21 14:58:12 -0800

    linked list new implementation

--- Linked_List.py
+++ Linked_List.py
@@ -1,34 +1,46 @@
 # Linked list is usually a list of nodes. And each node has a "Value" and a "Next"
 # where "Next" stores the address of the next node. That is how they can be chained
 # together. So a whole Linked List is essentially just chained up Nodes.
+# Program for traversal of linked list
 
-
-class Node:
+class ListNode:
 
     def __init__(self, data):
         self.data = data
         self.next = None
 
+    def __str__(self):
+        node = self
+        output = ""
+        while node:
+            output += str(node.data) + " "
+            node = node.next
+        return output
 
-class LinkedList:
 
-    def __init__(self):
-        self.head = None
+def list2linkedlist(l):
+    if l == []:
+        return None
+    head = ListNode(l[0])
+    n = head
+    for i in range(1, len(l)):
+        new = ListNode(l[i])
+        n.next = new
+        n = n.next
+    return head
 
 
 if __name__ == "__main__":
-    llist = LinkedList()
+    l1 = [1, 3, 5, 7, 9]
+    l2 = [2, 4, 6, 8, 10]
+    n1 = list2linkedlist(l1)
+    n2 = list2linkedlist(l2)
+    print (n1)
+    print (n2)
+    # adding a new node in n1
 
-    llist.head = Node(1)
-
-    second = Node(2)
-
-    third = Node(3)
-
-    llist.head.next = second
-
-    second.next = third
-
-    print(llist.head.data, llist.head)
-    print(second.data, second)
-    print(third.data, third)
+    print ("Lets add a new node in n1")
+    newnode = ListNode(1)
+    print (newnode)
+    newnode.next = n1
+    print (newnode)
