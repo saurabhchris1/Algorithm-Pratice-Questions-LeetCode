269a467ecd500c3a747656d7f2bc85e95d9beb10
Saurabh Jaiswal <saurabhchris1@gmail.com>
2019-12-25 17:07:57 -0800

    Group Anagrams

--- Group_Anagrams.py
+++ Group_Anagrams.py
@@ -13,3 +13,34 @@
 #
 # All inputs will be in lowercase.
 # The order of your output does not matter.
+
+# class Solution:
+#     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+#
+#         dict = collections.defaultdict(list)
+#
+#         def hashkey(anyword):
+#             arr = [0] * 26
+#             for char in anyword:
+#                 arr[ord(char) - ord('a')] = 1
+#             return tuple(arr)
+#
+#         for word in strs:
+#             dict[hashkey(word)].append(word)
+#
+#         return dict.values()
+
+import collections
+
+
+class Solution:
+    def groupAnagrams(self, strs):
+        dict = collections.defaultdict(list)
+
+        def hashkey(anyword):
+            return "".join(sorted(anyword))
+
+        for word in strs:
+            dict[hashkey(word)].append(word)
+
+        return dict.values()
