49a6a3cb68f4eb4c651154ed0664e8a643b0e041
Saurabh Jaiswal <saurabhchris1@gmail.com>
2019-12-31 20:39:38 -0800

    Symmetric Tree Iterative

--- Symmetric_Tree.py
+++ Symmetric_Tree.py
@@ -34,3 +34,28 @@ class Solution:
             return helper(node1.left, node2.right) and helper(node1.right, node2.left)
 
         return helper(root.left, root.right)
+
+    def isSymmetricIterative(self, root):
+
+        if not root:
+            return True
+
+        stack = []
+        stack.append([root.left, root.right])
+
+        while len(stack):
+            node1, node2 = stack.pop()
+
+            if not node1 and not node2:
+                continue
+
+            if not node1 or not node2:
+                return False
+
+            if node1.val != node2.val:
+                return False
+
+            stack.append([node1.left, node2.right])
+            stack.append([node1.right, node2.left])
+
+        return True
