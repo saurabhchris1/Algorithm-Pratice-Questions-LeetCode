2a9083cce6348e89982542766a3aa3bb97b8df38
Saurabh Jaiswal <saurabhchris1@gmail.com>
2020-05-22 12:09:18 -0700

    Order-agnostic Binary Search

---
+++ BinarySearch/BinSearchAscorDesc.py
@@ -0,0 +1,58 @@
+# Order-agnostic Binary Search
+# Given a sorted array of numbers, find if a given number key is present in the array. Though we know that
+# the array is sorted, we don't know if it's sorted in ascending or descending order. You should assume
+# that the array can have duplicates.
+#
+# Write a function to return the index of the 'key' if it is present in the array, otherwise return -1.
+#
+# Example 1:
+#
+# Input: [4, 6, 10], key = 10
+# Output: 2
+# Example 2:
+#
+# Input: [1, 2, 3, 4, 5, 6, 7], key = 5
+# Output: 4
+# Example 3:
+#
+# Input: [10, 6, 4], key = 10
+# Output: 0
+# Example 4:
+#
+# Input: [10, 6, 4], key = 4
+# Output: 2
+
+
+class Solution:
+    def binarySearch(self, nums, target):
+        start = 0
+        end = len(nums) - 1
+        is_ascending = True if nums[1] > nums[0] else False
+
+        while start <= end:
+            mid = start + (end - start) // 2
+
+            if nums[mid] == target:
+                return mid
+
+            if is_ascending:
+                if target > nums[mid]:
+                    start = mid + 1
+                else:
+                    end = mid - 1
+
+            else:
+                if target < nums[mid]:
+                    start = mid + 1
+                else:
+                    end = mid - 1
+
+                    return -1
+
+
+if __name__ == "__main__":
+    arr1 = [1, 2, 3, 4, 5, 6]
+    arr2 = [9, 8, 7, 6, 5, 4, 3]
+
+    print(Solution().binarySearch(arr1, 4))
+    print (Solution().binarySearch(arr2, 6))
