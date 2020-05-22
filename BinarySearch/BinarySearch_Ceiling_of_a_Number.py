9b5cde5e7c0fa462351b4613b3831066e5f746c4
Saurabh Jaiswal <saurabhchris1@gmail.com>
2020-05-22 14:20:35 -0700

    Ceiling of a Number (medium)

--- BinarySearch/BinarySearch_Ceiling_of_a_Number.py
+++ BinarySearch/BinarySearch_Ceiling_of_a_Number.py
@@ -24,10 +24,15 @@
 # Output: 0
 # Explanation: The smallest number greater than or equal to '-1' is '4' having index '0'.
 
+# Similar Problems
+# Problem 1
+# Given an array of numbers sorted in ascending order, find the floor of a given number key.
+# The floor of the key will be the biggest element in the given array smaller than or equal to the key
+# Write a function to return the index of the floor of the key. If there isn't a floor, return -1.
 
 class Solution:
 
-    def celing_of_num(self, nums, key):
+    def celing_of_num1(self, nums, key):
         n = len(nums) - 1
         start = 0
         end = n
@@ -48,3 +53,25 @@ class Solution:
                 start = mid + 1
 
         return start
+
+    def celing_of_num2(self, nums, key):
+        n = len(nums) - 1
+        start = 0
+        end = n
+
+        if key < nums[0]:
+            return -1
+
+        while start <= end:
+
+            mid = start + (end - start) // 2
+
+            if nums[mid] == key:
+                return mid
+
+            elif key < nums[mid]:
+                end = mid - 1
+            else:
+                start = mid + 1
+
+        return end
