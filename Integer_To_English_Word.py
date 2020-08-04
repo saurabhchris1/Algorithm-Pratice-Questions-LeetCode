03ca7304ed5ff6cb1266031f2a9c7194f6be21bf
Saurabh Jaiswal <saurabhchris1@gmail.com>
2020-08-03 18:14:16 -0700

    Integer To English Word

---
+++ Integer_To_English_Word.py
@@ -0,0 +1,112 @@
+# Convert a non-negative integer to its english words representation. Given input is
+# guaranteed to be less than 231 - 1.
+#
+# Example 1:
+#
+# Input: 123
+# Output: "One Hundred Twenty Three"
+# Example 2:
+#
+# Input: 12345
+# Output: "Twelve Thousand Three Hundred Forty Five"
+# Example 3:
+#
+# Input: 1234567
+# Output: "One Million Two Hundred Thirty Four Thousand Five Hundred Sixty Seven"
+# Example 4:
+#
+# Input: 1234567891
+# Output: "One Billion Two Hundred Thirty Four Million Five Hundred Sixty Seven " \
+#         "Thousand Eight Hundred Ninety One"
+
+
+class Solution:
+    def numberToWords(self, num):
+
+        def one(num):
+            switcher = {
+                1: 'One',
+                2: 'Two',
+                3: 'Three',
+                4: 'Four',
+                5: 'Five',
+                6: 'Six',
+                7: 'Seven',
+                8: 'Eight',
+                9: 'Nine'
+            }
+            return switcher.get(num)
+
+        def two_less_20(num):
+            switcher = {
+                10: 'Ten',
+                11: 'Eleven',
+                12: 'Twelve',
+                13: 'Thirteen',
+                14: 'Fourteen',
+                15: 'Fifteen',
+                16: 'Sixteen',
+                17: 'Seventeen',
+                18: 'Eighteen',
+                19: 'Nineteen'
+            }
+            return switcher.get(num)
+
+        def tenner(num):
+            switcher = {
+                2: 'Twenty',
+                3: 'Thirty',
+                4: 'Forty',
+                5: 'Fifty',
+                6: 'Sixty',
+                7: 'Seventy',
+                8: 'Eighty',
+                9: 'Ninety'
+            }
+            return switcher.get(num)
+
+        def two(num):
+            if not num:
+                return ''
+            elif num < 10:
+                return one(num)
+            elif num < 20:
+                return two_less_20(num)
+            else:
+                ten = num // 10
+                rest = num - ten * 10
+                return tenner(ten) + ' ' + one(rest) if rest else tenner(ten)
+
+        def three(num):
+            hundred = num // 100
+            rest = num - hundred * 100
+
+            if hundred and rest:
+                return one(hundred) + ' Hundred ' + two(rest)
+            elif not hundred and rest:
+                return two(rest)
+            elif hundred and not rest:
+                return one(hundred) + ' Hundred'
+
+        billion = num // 1000000000
+        million = (num - billion * 1000000000) // 1000000
+        thousand = (num - billion * 1000000000 - million * 1000000) // 1000
+        rest = num - billion * 1000000000 - million * 1000000 - thousand * 1000
+
+        result = ''
+        if not num:
+            return "Zero"
+
+        if billion:
+            result = three(billion) + ' Billion'
+        if million:
+            result += ' ' if result else ''
+            result += three(million) + ' Million'
+        if thousand:
+            result += ' ' if result else ''
+            result += three(thousand) + ' Thousand'
+        if rest:
+            result += ' ' if result else ''
+            result += three(rest)
+
+        return result
