01aa4ce711c6563b7aa128f22ae42ad01b99ace8
Saurabh Jaiswal <saurabhchris1@gmail.com>
2020-08-05 11:52:43 -0700

    Binary_Tree_Level_Order_Traversal recursive

--- Binary_Tree_Level_Order_Traversal.py
+++ Binary_Tree_Level_Order_Traversal.py
@@ -51,3 +51,27 @@ class Solution:
             res.append(arr)
 
         return res
+
+    def recursive(self, root):
+
+        levels = []
+
+        if not root:
+            return levels
+
+        def helper(node, level):
+
+            if len(levels) == level:
+                levels.append([])
+
+            levels[level].append(node.val)
+
+            if node.left:
+                helper(node.left, level + 1)
+
+            if node.right:
+                helper(node.right, level + 1)
+
+        helper(root, 0)
+
+        return levels
