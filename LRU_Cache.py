c135bb322fbda8505f85deaa9cfe3b9ed279a443
Saurabh Jaiswal <saurabhchris1@gmail.com>
2020-08-21 13:30:12 -0700

    LRU Cache

---
+++ LRU_Cache.py
@@ -0,0 +1,105 @@
+# Design and implement a data structure for Least Recently Used (LRU) cache.
+# It should support the following operations: get and put.
+#
+# get(key) - Get the value (will always be positive) of the key if the key
+# exists in the cache, otherwise return -1.
+# put(key, value) - Set or insert the value if the key is not already present.
+# When the cache reached its capacity, it should invalidate the least recently
+# used item before inserting a new item.
+#
+# The cache is initialized with a positive capacity.
+#
+# Follow up:
+# Could you do both operations in O(1) time complexity?
+#
+# Example:
+#
+# LRUCache cache = new LRUCache( 2 /* capacity */ );
+#
+# cache.put(1, 1);
+# cache.put(2, 2);
+# cache.get(1);       // returns 1
+# cache.put(3, 3);    // evicts key 2
+# cache.get(2);       // returns -1 (not found)
+# cache.put(4, 4);    // evicts key 1
+# cache.get(1);       // returns -1 (not found)
+# cache.get(3);       // returns 3
+# cache.get(4);       // returns 4
+
+
+class DLinkedNode:
+    def __init__(self):
+        self.key = 0
+        self.value = 0
+        self.prev = None
+        self.next = None
+
+
+class LRUCache:
+
+    def __init__(self, capacity):
+        self.cache = {}
+        self.size = 0
+        self.capacity = capacity
+        self.head = DLinkedNode()
+        self.tail = DLinkedNode()
+        self.head.next = self.tail
+        self.tail.prev = self.head
+
+    def _add_node(self, node):
+        node.prev = self.head
+        node.next = self.head.next
+        self.head.next.prev = node
+        self.head.next = node
+
+    def _remove_node(self, node):
+        prev = node.prev
+        new = node.next
+
+        prev.next = new
+        new.prev = prev
+
+        node.prev = None
+        node.next = None
+
+    def _move_to_head(self, node):
+        self._remove_node(node)
+        self._add_node(node)
+
+    def _pop_tail(self):
+        res = self.tail.prev
+        self._remove_node(res)
+        return res
+
+    def get(self, key):
+        node = self.cache.get(key, None)
+
+        if not node:
+            return -1
+        self._move_to_head(node)
+
+        return node.value
+
+    def put(self, key, value):
+        node = self.cache.get(key, None)
+
+        if not node:
+            newNode = DLinkedNode()
+            newNode.key = key
+            newNode.value = value
+            self.cache[key] = newNode
+            self._add_node(newNode)
+            self.size += 1
+
+            if self.size > self.capacity:
+                tail = self._pop_tail()
+                del self.cache[tail.key]
+                self.size -= 1
+        else:
+            node.value = value
+            self._move_to_head(node)
+
+# Your LRUCache object will be instantiated and called as such:
+# obj = LRUCache(capacity)
+# param_1 = obj.get(key)
+# obj.put(key,value)
