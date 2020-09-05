c452b798ffdada48e507c362340fbeca0ea127cf
Saurabh Jaiswal <saurabhchris1@gmail.com>
2020-09-05 14:29:06 -0700

    Partition Linked List

---
+++ Partition_List_or_Linked_List.py
@@ -0,0 +1,43 @@
+# Given a linked list and a value x, partition it such that all nodes
+# less than x come before nodes greater than or equal to x.
+#
+# You should preserve the original relative order of the nodes
+# in each of the two partitions.
+#
+# Example:
+#
+# Input: head = 1->4->3->2->5->2, x = 3
+# Output: 1->2->2->4->3->5
+
+# Definition for singly-linked list.
+
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+
+class Solution:
+    def partition(self, head, x):
+
+        before_head = ListNode(0)
+        after_head = ListNode(0)
+        before = before_head
+        after = after_head
+
+        while head:
+
+            if head.val < x:
+                before.next = head
+                before = before.next
+            else:
+                after.next = head
+                after = after.next
+
+            head = head.next
+
+        after.next = None
+
+        before.next = after_head.next
+
+        return before_head.next
