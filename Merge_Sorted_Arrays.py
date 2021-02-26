60dbe2d39d6d15fd8a3a8362367068720f7093d6
Saurabh Jaiswal <saurabhchris1@gmail.com>
2021-02-25 19:53:56 -0800

    Optimized the code Merge Sorted Arrays

--- Merge_Sorted_Arrays.py
+++ Merge_Sorted_Arrays.py
@@ -17,31 +17,16 @@
 
 class Solution:
     def merge(self, nums1, m, nums2, n):
-        if not nums1:
-            return nums2
-        if not nums2:
-            return nums1
-
-        end = len(nums1) - 1
         p1 = m - 1
         p2 = n - 1
 
-        while p1 >= 0 and p2 >= 0 and end >= 0:
+        for p in range(m + n - 1, -1, -1):
 
-            if nums1[p1] >= nums2[p2]:
-                nums1[end] = nums1[p1]
-                end -= 1
+            if p2 < 0:
+                break
+            if p1 >= 0 and nums1[p1] > nums2[p2]:
+                nums1[p] = nums1[p1]
                 p1 -= 1
             else:
-                nums1[end] = nums2[p2]
+                nums1[p] = nums2[p2]
                 p2 -= 1
-                end -= 1
-        while p1 >= 0 and end >= 0:
-            nums1[end] = nums1[p1]
-            p1 -= 1
-            end -= 1
-
-        while p2 >= 0 and end >= 0:
-            nums1[end] = nums2[p2]
-            p2 -= 1
-            end -= 1
