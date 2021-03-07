0563ec6f0e9dbb88a60af39e93563df63b9b93f6
Saurabh Jaiswal <saurabhchris1@gmail.com>
2021-03-07 11:40:08 -0800

    Populating Next Right Pointers in Each Node

---
+++ Populating_Next_Right_Pointers_in_Each_Node.py
@@ -0,0 +1,53 @@
+# You are given a perfect binary tree where all leaves are on the
+# same level, and every parent has two children. The binary tree
+# has the following definition:
+#
+# Populate each next pointer to point to its next right node. If
+# there is no next right node, the next pointer should be set to NULL.
+#
+# Initially, all next pointers are set to NULL.
+#
+# Follow up:
+#
+# You may only use constant extra space.
+# Recursive approach is fine, you may assume implicit stack space
+# does not count as extra space for this problem.
+#
+
+# """
+# # Definition for a Node.
+# class Node:
+#     def __init__(self, val: int = 0, left: 'Node' = None, right: 'Node' = None, next: 'Node' = None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+#         self.next = next
+# """
+
+from collections import deque
+
+
+class Solution:
+    def connect(self, root):
+        if not root:
+            return None
+        queue = deque()
+        queue.append(root)
+
+        while queue:
+            num = len(queue)
+
+            for i in range(num):
+                node = queue.popleft()
+                if i + 1 == num:
+                    node.next = None
+                else:
+                    nextNode = queue[0]
+                    node.next = nextNode
+
+                if node.left:
+                    queue.append(node.left)
+                if node.right:
+                    queue.append(node.right)
+
+        return root
