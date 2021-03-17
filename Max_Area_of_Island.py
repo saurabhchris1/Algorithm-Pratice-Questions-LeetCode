607834d69eec7471e7037b11e2ad14dfa1427505
Saurabh Jaiswal <saurabhchris1@gmail.com>
2021-03-17 16:36:13 -0700

    Max Area of Island Iterative

--- Max_Area_of_Island.py
+++ Max_Area_of_Island.py
@@ -17,33 +17,63 @@
 
 
 class Solution:
+
     def maxAreaOfIsland(self, grid):
         rows = len(grid)
         cols = len(grid[0])
         maxArea = 0
+        seen = set()
+
         for row in range(rows):
             for col in range(cols):
-                currentMax = 0
-                if grid[row][col] == 1:
-                    currentMax = self.findArea(grid, row, col, currentMax)
+                if grid[row][col] == 1 and (row, col) not in seen:
+                    currentMax = 0
+                    stack = []
+                    stack.append((row, col))
 
-                maxArea = max(maxArea, currentMax)
+                    while stack:
+                        r, c = stack.pop()
+                        if grid[r][c] == 1 and (r, c) not in seen:
+                            currentMax += 1
+                            seen.add((r, c))
+                            directions = [(0, 1), (0, -1), (1, 0), (-1, 0)]
+
+                            for d in directions:
+                                newRow = r + d[0]
+                                newCol = c + d[1]
+                                if 0 <= newRow < len(grid) and 0 <= newCol < len(grid[0]):
+                                    stack.append((newRow, newCol))
+
+                    maxArea = max(maxArea, currentMax)
         return maxArea
 
-    def findArea(self, grid, row, col, currentMax):
-        if grid[row][col] == 1:
-            currentMax += 1
-            grid[row][col] = 0
-        else:
-            return currentMax
-
-        directions = [(1, 0), (0, 1), (-1, 0), (0, -1)]
-
-        for d in directions:
-            r = row + d[0]
-            c = col + d[1]
-
-            if 0 <= r < len(grid) and 0 <= c < len(grid[0]):
-                currentMax = self.findArea(grid, r, c, currentMax)
-
-        return currentMax
+    # def maxAreaOfIsland(self, grid):
+    #     rows = len(grid)
+    #     cols = len(grid[0])
+    #     maxArea = 0
+    #     for row in range(rows):
+    #         for col in range(cols):
+    #             currentMax = 0
+    #             if grid[row][col] == 1:
+    #                 currentMax = self.findArea(grid, row, col, currentMax)
+    #
+    #             maxArea = max(maxArea, currentMax)
+    #     return maxArea
+    #
+    # def findArea(self, grid, row, col, currentMax):
+    #     if grid[row][col] == 1:
+    #         currentMax += 1
+    #         grid[row][col] = 0
+    #     else:
+    #         return currentMax
+    #
+    #     directions = [(1, 0), (0, 1), (-1, 0), (0, -1)]
+    #
+    #     for d in directions:
+    #         r = row + d[0]
+    #         c = col + d[1]
+    #
+    #         if 0 <= r < len(grid) and 0 <= c < len(grid[0]):
+    #             currentMax = self.findArea(grid, r, c, currentMax)
+    #
+    #     return currentMax
