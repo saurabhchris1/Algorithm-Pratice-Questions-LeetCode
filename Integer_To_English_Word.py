018adcfb1aa8c74daf58b15b7d5129cad8170cb7
Saurabh Jaiswal <saurabhchris1@gmail.com>
2021-11-24 15:07:21 -0800

    Update the implementation

--- Integer_To_English_Word.py
+++ Integer_To_English_Word.py
@@ -21,49 +21,52 @@
 
 
 class Solution:
-    def numberToWords(self, num):
+    def numberToWords(self, num: int) -> str:
 
         def one(num):
-            switcher = {
-                1: 'One',
-                2: 'Two',
-                3: 'Three',
-                4: 'Four',
-                5: 'Five',
-                6: 'Six',
-                7: 'Seven',
-                8: 'Eight',
-                9: 'Nine'
+            dict = {
+                1: "One",
+                2: "Two",
+                3: "Three",
+                4: "Four",
+                5: "Five",
+                6: "Six",
+                7: "Seven",
+                8: "Eight",
+                9: "Nine"
             }
-            return switcher.get(num)
+
+            return dict[num]
 
         def two_less_20(num):
-            switcher = {
-                10: 'Ten',
-                11: 'Eleven',
-                12: 'Twelve',
-                13: 'Thirteen',
-                14: 'Fourteen',
-                15: 'Fifteen',
-                16: 'Sixteen',
-                17: 'Seventeen',
-                18: 'Eighteen',
-                19: 'Nineteen'
+            dict = {
+                10: "Ten",
+                11: "Eleven",
+                12: "Twelve",
+                13: "Thirteen",
+                14: "Fourteen",
+                15: "Fifteen",
+                16: "Sixteen",
+                17: "Seventeen",
+                18: "Eighteen",
+                19: "Nineteen"
             }
-            return switcher.get(num)
 
-        def tenner(num):
-            switcher = {
-                2: 'Twenty',
-                3: 'Thirty',
-                4: 'Forty',
-                5: 'Fifty',
-                6: 'Sixty',
-                7: 'Seventy',
-                8: 'Eighty',
-                9: 'Ninety'
+            return dict[num]
+
+        def ten(num):
+            dict = {
+                2: "Twenty",
+                3: "Thirty",
+                4: "Forty",
+                5: "Fifty",
+                6: "Sixty",
+                7: "Seventy",
+                8: "Eighty",
+                9: "Ninety"
             }
-            return switcher.get(num)
+
+            return dict[num]
 
         def two(num):
             if not num:
@@ -73,16 +76,17 @@ class Solution:
             elif num < 20:
                 return two_less_20(num)
             else:
-                ten = num // 10
-                rest = num - ten * 10
-                return tenner(ten) + ' ' + one(rest) if rest else tenner(ten)
+                tenner = num // 10
+                rest = num - tenner * 10
+
+                return ten(tenner) + ' ' + one(rest) if rest else ten(tenner)
 
         def three(num):
             hundred = num // 100
             rest = num - hundred * 100
 
             if hundred and rest:
-                return one(hundred) + ' Hundred ' + two(rest)
+                return one(hundred) + " Hundred " + two(rest)
             elif not hundred and rest:
                 return two(rest)
             elif hundred and not rest:
@@ -91,22 +95,23 @@ class Solution:
         billion = num // 1000000000
         million = (num - billion * 1000000000) // 1000000
         thousand = (num - billion * 1000000000 - million * 1000000) // 1000
-        rest = num - billion * 1000000000 - million * 1000000 - thousand * 1000
+        rest = (num - billion * 1000000000 - million * 1000000 - thousand * 1000)
 
-        result = ''
         if not num:
             return "Zero"
 
+        result = ""
         if billion:
-            result = three(billion) + ' Billion'
+            result += three(billion) + " Billion"
         if million:
-            result += ' ' if result else ''
-            result += three(million) + ' Million'
+            result += " " if result else ""
+            result += three(million) + " Million"
         if thousand:
-            result += ' ' if result else ''
-            result += three(thousand) + ' Thousand'
+            result += " " if result else ""
+            result += three(thousand) + " Thousand"
         if rest:
-            result += ' ' if result else ''
+            result += " " if result else ""
             result += three(rest)
 
         return result
+
