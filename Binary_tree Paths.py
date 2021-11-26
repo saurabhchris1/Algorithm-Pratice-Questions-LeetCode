4d536d392a12409606125cd904e9086f3a31e825
Saurabh Jaiswal <saurabhchris1@gmail.com>
2021-11-25 21:46:37 -0800

    Binary Tree Paths iterative

--- Binary_tree Paths.py
+++ Binary_tree Paths.py
@@ -42,3 +42,21 @@ class Solution:
 
         helper(root, path)
         return res
+
+    def binaryTreePathsIterative(self, root):
+        res = []
+        stack = [(root, [str(root.val)])]
+
+        while stack:
+
+            node, path = stack.pop()
+
+            if not node.left and not node.right:
+                res.append("->".join(path))
+
+            if node.right:
+                stack.append((node.right, path + [str(node.right.val)]))
+            if node.left:
+                stack.append((node.left, path + [str(node.left.val)]))
+
+        return res
