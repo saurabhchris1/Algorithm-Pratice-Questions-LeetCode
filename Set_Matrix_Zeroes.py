2dcf5caf38f4755a6b1fe6d1f854a07effd196f0
Saurabh Jaiswal <saurabhchris1@gmail.com>
2021-11-26 20:37:31 -0800

    Set Matrix to Zero In place solution

--- Set_Matrix_Zeroes.py
+++ Set_Matrix_Zeroes.py
@@ -10,7 +10,7 @@
 # Output: [[0,0,0,0],[0,4,5,0],[0,3,1,0]]
 
 class Solution:
-    def setZeroes(self, matrix):
+    def setZero(self, matrix):
 
         rowSet = set()
         colSet = set()
@@ -28,4 +28,42 @@ class Solution:
                     matrix[r][c] = 0
                 if c in colSet:
                     matrix[r][c] = 0
+        return matrix
+
+    # Inplace o(1) space solution
+
+    def setZeroes(self, matrix):
+
+        isFirstRow = False
+        isFirstCol = False
+
+        for r in range(len(matrix)):
+            if matrix[r][0] == 0:
+                isFirstCol = True
+
+        for c in range(len(matrix[0])):
+            if matrix[0][c] == 0:
+                isFirstRow = True
+
+        for row in range(1, len(matrix)):
+            for col in range(1, len(matrix[0])):
+
+                if matrix[row][col] == 0:
+                    matrix[row][0] = 0
+                    matrix[0][col] = 0
+
+        for row in range(1, len(matrix)):
+            for col in range(1, len(matrix[0])):
+
+                if matrix[0][col] == 0 or matrix[row][0] == 0:
+                    matrix[row][col] = 0
+
+        if isFirstRow:
+            for c in range(len(matrix[0])):
+                matrix[0][c] = 0
+
+        if isFirstCol:
+            for r in range(len(matrix)):
+                matrix[r][0] = 0
+
         return matrix
