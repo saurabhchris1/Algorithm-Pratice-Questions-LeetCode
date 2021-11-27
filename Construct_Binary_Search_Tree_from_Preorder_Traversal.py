3ab73b567000846c0b60487e16c02ad19a9fc1b8
Saurabh Jaiswal <saurabhchris1@gmail.com>
2021-11-26 18:11:51 -0800

    Construct_Binary_Search_Tree_from_Preorder_Traversal iterative

--- Construct_Binary_Search_Tree_from_Preorder_Traversal.py
+++ Construct_Binary_Search_Tree_from_Preorder_Traversal.py
@@ -48,3 +48,25 @@ class Solution:
             return node
 
         return helper(float("-inf"), float("inf"))
+
+    def bstFromPreorderIterative(self, preorder):
+        if not preorder:
+            return None
+
+        root = TreeNode(preorder[0])
+        stack = [root]
+
+        for i in range(1, len(preorder)):
+            node = stack[-1]
+            child = TreeNode(preorder[i])
+
+            while stack and stack[-1].val < child.val:
+                node = stack.pop()
+
+            if child.val > node.val:
+                node.right = child
+            else:
+                node.left = child
+            stack.append(child)
+
+        return root
