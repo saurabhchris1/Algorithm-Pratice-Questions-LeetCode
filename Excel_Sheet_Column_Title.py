bef59b8e5c190ab061633bd280c27f21bf3228c1
Saurabh Jaiswal <saurabhchris1@gmail.com>
2021-12-06 14:50:57 -0800

    updated the implementation of excel sheet column number to column title

--- Excel_Sheet_Column_Title.py
+++ Excel_Sheet_Column_Title.py
@@ -26,46 +26,11 @@
 
 class Solution:
     def convertToTitle(self, n):
-        dict = {0: "Z",
-                1: "A",
-                2: "B",
-                3: "C",
-                4: "D",
-                5: "E",
-                6: "F",
-                7: "G",
-                8: "H",
-                9: "I",
-                10: "J",
-                11: "K",
-                12: "L",
-                13: "M",
-                14: "N",
-                15: "O",
-                16: "P",
-                17: "Q",
-                18: "R",
-                19: "S",
-                20: "T",
-                21: "U",
-                22: "V",
-                23: "W",
-                24: "X",
-                25: "Y"
-                }
-        arr = []
+        res = []
 
-        if n <= 26:
-            return dict[n % 26]
+        while columnNumber > 0:
+            right = (columnNumber - 1) % 26
+            columnNumber = (columnNumber - 1) // 26
+            res.append(chr(65 + right % 26))
 
-        while n > 0:
-            r = n % 26
-
-            n = n // 26
-
-            if (r == 0):
-                n = n - 1
-
-            arr = [dict[r]] + arr
-
-        return "".join(arr)
+        return "".join(res[::-1])
